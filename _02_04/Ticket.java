f0b262532a215575090080f4bc8698ca8144fe06
Komban <96132858+K0mban@users.noreply.github.com>
2024-02-22 18:48:16 +0000

    ticket completed

--- _02_04/Ticket.java
+++ _02_04/Ticket.java
@@ -11,9 +11,29 @@ public class Ticket {
   private boolean isReturn;
 
   // Add three methods to set the value of each field, called setDestination,
+
+  public void setDestination(String destination){
+    this.destination = destination;
+  }
+
+  public void setPrice(double price){
+    this.price = price;
+  }
+  public void setIsReturn(boolean isReturn){
+    this.isReturn = isReturn;
+  }
   // setPrice and setIsReturn.
 
   // Add a separate method to get the value of each field, called getDestination,
   // getPrice and getIsReturn.
+  public String getDestination(){
+    return destination;
+  }
+  public double getPrice(){
+    return price;
+  }
+  public getIsReturn(){
+    return isReturn;
+  }
 
 }
